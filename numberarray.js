417ccd1064fc476f979b9fc1de4b9db3c7a4fdc6
Lucas Wilson <lucaswil291@gmail.com>
2025-06-23 11:09:48 +1000

    Update numberarray.js

--- numberarray.js
+++ numberarray.js
@@ -1,8 +1,11 @@
 // Create the array
 let numbers = [11, 5, 8, 3, 25, 16, 31, 45, 14, 20];
 
-// Add the new numbers
+// Add the new numbers (from previous instruction)
 numbers.push(19, 23, 30);
+
+// Remove the numbers 8 and 31
+numbers = numbers.filter(number => number !== 8 && number !== 31);
 
 // Sort the array in ascending order
 numbers.sort((a, b) => a - b);
