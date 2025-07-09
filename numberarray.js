b06652a586d60c5858007d29fe215821bb2d3965
Lucas Wilson <lucaswil291@gmail.com>
2025-07-09 18:11:34 +1000

    Delete numberarray.js

--- numberarray.js
+++
@@ -1,8 +0,0 @@
-// Create the array
-let numbers = [11, 5, 8, 3, 25, 16, 31, 45, 14, 20];
-
-// Sort the array in ascending order
-numbers.sort((a, b) => a - b);
-
-// Print the sorted array to the console
-console.log(numbers);
