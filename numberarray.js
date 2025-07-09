5e086e78242a2518c3d28bceb6186f19b838eaa9
Lucas Wilson <lucaswil291@gmail.com>
2025-07-09 18:12:22 +1000

    Add files via upload

---
+++ numberarray.js
@@ -0,0 +1,18 @@
+// This script creates an array with the specified numbers.
+
+/**
+ * An array containing a list of numbers.
+ * @type {number[]}
+ */
+const numbers = [11, 5, 8, 3, 25, 16, 31, 45, 14, 20];
+
+// You can log the array to the console to see its content.
+console.log("The array of numbers is:", numbers);
+
+// You can also access individual elements, for example, the first element:
+// console.log("The first number is:", numbers[0]); // Output: 11
+
+// Or iterate through the array:
+// numbers.forEach(number => {
+//   console.log(number);
+// });
