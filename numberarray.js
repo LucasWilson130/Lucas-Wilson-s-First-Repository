8ee74c2c51d524e78abe8822fd8f006b84b3c90e
Lucas Wilson <lucaswil291@gmail.com>
2025-07-09 18:10:00 +1000

    Delete numberarray.js

--- numberarray.js
+++
@@ -1,8 +0,0 @@
-function sequentialSearch(arr, target) {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === target) {
-            return i; // Return the index if found
-        }
-    }
-    return -1; // Return -1 if not found
-}
