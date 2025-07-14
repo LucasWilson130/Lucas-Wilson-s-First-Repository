e1956c093d3d61ee79f41ea231cba6a21aa5aa60
Lucas Wilson <lucaswil291@gmail.com>
2025-07-14 17:57:12 +1000

    Add files via upload

---
+++ myMovie.js
@@ -0,0 +1,12 @@
+// Define the 'myMovie' object with the specified properties and data.
+const myMovie = {
+  Title: "The longest day",
+  Year: 1964,
+};
+
+// You can log the object to the console to verify its contents.
+console.log(myMovie);
+
+// You can access properties like this:
+// console.log(myMovie.Title); // Output: The longest day
+// console.log(myMovie.Year);  // Output: 1964
